6e24e1da3e291bc439fbb7c3cc98a89ee9654968
84monica <up201905753@fe.up.pt>
2022-04-28 22:59:17 +0100

    Add list of Classes to Students Screen

--- absence_counter/lib/screens/students_main_screen/item.dart
+++ absence_counter/lib/screens/students_main_screen/item.dart
@@ -1,16 +1,48 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Item extends StatelessWidget {
+  String _class_name, _teacher_name, _time_scheadule, _class_type;
+  int _number_of_absences;
+
+  Item(this._class_name, this._number_of_absences, this._teacher_name, this._time_scheadule, this._class_type);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("editar caixas",
-          style: TextStyle(
-              color: Colors.red,
-              fontWeight: FontWeight.w300,
-              fontSize: 30.0,
-              fontStyle: FontStyle.normal,
-              fontFamily: "OpenSans")),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(_time_scheadule,
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 15.0,
+                  fontStyle: FontStyle.normal,
+                  fontFamily: "OpenSans")),
+          Text(_class_name,
+              style: TextStyle(
+                  color: Colors.red.shade900,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 30.0,
+                  fontStyle: FontStyle.normal,
+                  fontFamily: "OpenSans")),
+          Text(_class_type+"\n"+_teacher_name,
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 15.0,
+                  fontStyle: FontStyle.normal,
+                  fontFamily: "OpenSans")),
+          Text(_number_of_absences.toString()+"/3",
+              style: TextStyle(
+                  color: Colors.red.shade900,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 25.0,
+                  fontStyle: FontStyle.normal,
+                  fontFamily: "OpenSans"))
+        ],
+      ),
       margin: const EdgeInsets.only(top: 20),
       alignment: Alignment.center,
       width: 300,
--- absence_counter/lib/screens/students_main_screen/list.dart
+++ absence_counter/lib/screens/students_main_screen/list.dart
@@ -7,11 +7,11 @@ class List extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Item(),
-        Item(),
-        Item(),
-        Item(),
-        Item(),
+        Item("ASSO", 2, "AMA", "17:00\n18:00", "(TP)"),
+        Item("CPAR", 3, "JGB", "10:00\n13:00", "(TP)"),
+        Item("LC", 1, "MMFC", "14:00\n16:00", "(PL)"),
+        Item("DA", 0, "FMMR", "16:00\n18:00", "(TP)"),
+        Item("ESOF", 0, "FBG", "19:00\n20:00", "(PL)"),
       ],
     );
   }
