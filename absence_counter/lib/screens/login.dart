d3efaa6e92318ddbd50f14323d7f06fad6cc9796
84monica <up201905753@fe.up.pt>
2022-05-26 16:15:16 +0100

    Changes to Login Design

--- absence_counter/lib/screens/login.dart
+++ absence_counter/lib/screens/login.dart
@@ -1,19 +1,21 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_first_flutter/screens/menu.dart';
 import 'package:my_first_flutter/screens/profile.dart';
+import 'package:my_first_flutter/screens/teacher_main_screen/teachers_screen.dart';
 
-
-class LoginForm extends StatefulWidget{
+class LoginForm extends StatefulWidget {
   @override
-
-  _LoginFormState createState()=>_LoginFormState();
+  _LoginFormState createState() => _LoginFormState();
 }
 
-class _LoginFormState extends State<LoginForm>{
-  bool ishiddenPassword=true;
+class _LoginFormState extends State<LoginForm> {
+  bool ishiddenPassword = true;
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       drawer: Menu(),
       appBar: AppBar(
@@ -25,47 +27,67 @@ class _LoginFormState extends State<LoginForm>{
         children: [
           Text(
             'UNI',
-                style:Theme.of(context).textTheme.headline3,
+            style: Theme.of(context).textTheme.headline3,
+          ),
+
+          TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+                hintText: 'Email',
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50.0))),
           ),
           TextField(
+            obscureText: ishiddenPassword,
             decoration: InputDecoration(
-                hintText:'número de estudante'
-            ),
-          ),
-          TextField(
-            obscureText:ishiddenPassword,
-            decoration: InputDecoration(
-                hintText:'Password',
-                    suffixIcon:InkWell(
-                      onTap:_togglePasswordView,
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(50.0)),
+              hintText: 'Password',
+              suffixIcon: InkWell(
+                onTap: _togglePasswordView,
                 child: Icon(
                   Icons.visibility,
                 ),
-            ),
+              ),
             ),
           ),
           FlatButton(
             color: Theme.of(context).primaryColor,
-            onPressed: (){},
             child: Text(
               'Entrar',
-            style:TextStyle(
-              color:Colors.white,
+              style: TextStyle(
+                color: Colors.white,
+              ),
             ),
-            ),
-          )
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50.0),
+                side: BorderSide(color: Colors.red.shade900)),
+            onPressed: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => TeacherScreen()));
+            },
 
+          ),
+          FlatButton(
+              child: Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+              ),
+              onPressed: null)
         ],
       ),
     );
   }
-  void _togglePasswordView(){
-    if(ishiddenPassword==true){
-      ishiddenPassword=false;
-    }
-    else{
-      ishiddenPassword=true;
-    }
 
+  void _togglePasswordView() {
+    if (ishiddenPassword == true) {
+      ishiddenPassword = false;
+    } else {
+      ishiddenPassword = true;
+    }
   }
 }
