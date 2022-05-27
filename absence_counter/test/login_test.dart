fae55eba9a4ccca7cb512e7317e24912e5ba914e
84monica <up201905753@fe.up.pt>
2022-05-27 16:43:54 +0100

    Add Login Test and CheckBoxTest

---
+++ absence_counter/test/login_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:my_first_flutter/screens/login.dart';
+
+void main() {
+
+  Widget createWidgetForTesting({Widget? child}){
+    return MaterialApp(
+      home: child,
+    );
+  }
+
+  testWidgets('Login Page test', (WidgetTester tester) async {
+    await tester.pumpWidget(createWidgetForTesting(child: new LoginForm()));
+    await tester.pumpAndSettle();
+    var textField = find.byType(TextField);
+    expect(textField, findsWidgets);
+  });
+}
